79dede5544dbdd9199a6c8c3617165e46cf29e0d
Gabriella Jacubavicius <gabriella_jacubavicius@hotmail.com>
2024-07-20 18:14:16 -0300

    commit de teste

--- helloWorld/helloWorld/src/main/java/com/generation/helloWorld/HelloWorldApplication.java
+++ helloWorld/helloWorld/src/main/java/com/generation/helloWorld/HelloWorldApplication.java
@@ -3,11 +3,22 @@ package com.generation.helloWorld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.generation.helloWorld.controller.TestesController;
+
 @SpringBootApplication
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
+		
+		int teste =5;
+		int testesb = 7;
+		
+		int soma = soma(teste, testesb);
+		
 	}
 
+	public static int soma(int a, int b) {
+		return a + b;
+	}
 }
